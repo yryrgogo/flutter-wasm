e4032bf67e3db27cc60b86a200f7ea3386938213
yryrgogo <yryrgogo@gmail.com>
2023-05-06 08:23:15 +0900

    :+1: add Questmissions model

--- rust/edge/src/main.rs
+++ rust/edge/src/main.rs
@@ -1,16 +1,5 @@
-use serde::Serialize;
-
 mod db;
 mod models;
-
-#[derive(Debug, Serialize)]
-struct Quest {
-    quest_id: i32,
-    quest_name: String,
-    description: String,
-    objective: String,
-    reward_id: Option<i32>,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
